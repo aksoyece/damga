f4ee8aed22a3eeab4f1dc7fa3726271e02c99cc8
aksoyece <aksoyece241@gmail.com>
2026-08-31 16:49:40 +0300

    fix: haritada tekerlek ile zoom geri acildi
    Harita uzerinde scroll zoom korunur; sayfa kaydirma ve orta tus duzeltmesi aynen kalir.

--- components/Map.vue
+++ components/Map.vue
@@ -110,7 +110,7 @@ async function initMap() {
     map = L.map(mapContainer.value, {
       preferCanvas: true,
       zoomControl: true,
-      scrollWheelZoom: false,
+      scrollWheelZoom: true,
       fadeAnimation: false,
       markerZoomAnimation: false,
       zoomAnimation: false,
